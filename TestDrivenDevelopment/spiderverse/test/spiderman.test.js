7ed5ae262d0fc2c98ec35709c3966e8172f03229
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-19 00:53:01 -0500

    III. TDD. Step 1: Writing test

--- TestDrivenDevelopment/spiderverse/test/spiderman.test.js
+++ TestDrivenDevelopment/spiderverse/test/spiderman.test.js
@@ -1,7 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2;
+describe("Unit Tests for Spider-Man Class", () => {
+    test('1) Create a Spider-Man Object', () => {
+        // spiderMan instance
+        const tobeySpiderMan = new spiderMan("First Spider-Man", 47, "Tobey Maguire", 3, "Marvel Enterprises");
 
-        expect(resultOfSomething).toBe(3);
+        // Validating code
+        expect(tobeySpiderMan.name).toBe("First Spider-Man")
+        expect(tobeySpiderMan.age).toBe(25)
+        expect(tobeySpiderMan.actor).toBe("Tobey Maguire")
+        expect(tobeySpiderMan.numberOfMovies).toBe(3)
+        expect(tobeySpiderMan.studio).toBe("Marvel Enterprises")
     });
 })
