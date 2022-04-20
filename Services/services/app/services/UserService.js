9e44df0606dafa24ab2f88fbb595eb837bbebfbd
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-20 01:55:39 -0500

    2. Requirement 2. Adding userInfoInList method and corresponding unit test

--- Services/services/app/services/UserService.js
+++ Services/services/app/services/UserService.js
@@ -2,7 +2,11 @@ const User = require('../models/User');
 
 class UserService {
     static create(id, username, name) {
-        return new User(id, username, name, "No bio");
+        return new User(id, username, name, "No Bio");
+    }
+
+    static getInfo(user) {
+        return [user.id, user.username, user.name, user.bio];
     }
 }
 
