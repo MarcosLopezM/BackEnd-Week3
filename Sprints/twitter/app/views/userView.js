0ed4ed01551ab876c1d42860149ed68cbfb5dc41
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-21 17:34:17 -0500

    Step 3. Requirement 2. Adding new condition to createUser method that verifies if any of payload's properties is null

--- Sprints/twitter/app/views/userView.js
+++ Sprints/twitter/app/views/userView.js
@@ -1,8 +1,9 @@
 class UserView{
     static createUser(payload){
         if(payload === null){
-            console.log("Error es null")
             return {error: "El payload no existe."}
+        } else if (payload.username === null || payload.name === null || payload.id === null) {
+            return {error: "necesitan tener un valor válido."}
         }
     }
 }
