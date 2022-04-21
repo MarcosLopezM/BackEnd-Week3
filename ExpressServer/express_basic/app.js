59d6ed3db7be3fdfa5208125e930b3524aa7d5a6
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-21 18:32:51 -0500

    Step 1. Creating my first server

---
+++ ExpressServer/express_basic/app.js
@@ -0,0 +1,18 @@
+// Usando objeto express
+const express = require('express');
+
+// App de express
+const app = express();
+
+// Puerto en el que vamos a ver nuestra app: localhost:3000
+const port = 3000;
+
+// path inicial, responderá a la url localhost:3000
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+// Con esto inicializamos la app
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
